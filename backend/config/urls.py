2a8970427fc66db3123538455aaa183cbc9efe4c
AhmedZulkarnaev <ahmedzulkarnaev9500@gmail.com>
2025-08-15 23:49:19 +0300

    Добавление url документации

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("expenses.api.urls")),
     path("api/auth/", include("users.urls")),
-    path("api/", include("expenses.urls")),
+    # OpenAPI JSON-схема
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # UI для схемы
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
